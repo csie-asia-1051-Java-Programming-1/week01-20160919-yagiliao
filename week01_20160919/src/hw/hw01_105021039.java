6a908ee4906f121bbb514dc6927f2b9c1986ef71
yagiliao <105021039@live.asia.edu.tw>
2016-09-19 15:57:38 +0800

    hw

--- week01_20160919/src/hw/hw01_105021039.java
+++ week01_20160919/src/hw/hw01_105021039.java
@@ -4,7 +4,9 @@ public class hw01_105021039 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scr=new Scanner(System.in);
+		int n = scr.nextInt();
+		System.out.println(Math.ceil(Math.log(n)/Math.log(2)));
 	}
 
 }
